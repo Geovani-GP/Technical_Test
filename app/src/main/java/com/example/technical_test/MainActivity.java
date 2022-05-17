f42bb8aace711cc7204df757df67cf3e4b8b5ac6
Geovani Gómez Pérez <l.a.web@hotmail.com>
2022-05-17 15:48:01 -0500

    Refactor, code and finish method post, for insert data.

--- app/src/main/java/com/example/technical_test/MainActivity.java
+++ app/src/main/java/com/example/technical_test/MainActivity.java
@@ -3,23 +3,44 @@ package com.example.technical_test;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.technical_test.Options.BasicAuthInterceptor;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
-private EditText et_nombre;
+    public OkHttpClient client;
+
+    public static final MediaType CONTENT_TYPE = MediaType.get("text/plain");
+    String apURL = "https://apidev.buroidentidad.com:9425/uaa/oauth/token?grant_type=password&username=training&password=password";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et_nombre = findViewById(R.id.et_Name);
+        EditText et_nombre = findViewById(R.id.et_Name);
         Button btnSiguiente = findViewById(R.id.btn_Siguiente);
         et_nombre.addTextChangedListener(new TextWatcher() {
             @Override
@@ -31,18 +52,72 @@ private EditText et_nombre;
             }
 
             public void afterTextChanged(Editable s) {
-                if (s.toString().length() > 0) {
-                    btnSiguiente.setEnabled(true);
-                } else {
-                    btnSiguiente.setEnabled(false);
-                }
+                btnSiguiente.setEnabled(s.toString().length() > 0);
             }
         });
     }
 
 
-    public void next_screen(View v){
-        Intent intent = new Intent(this, MainActivity2.class);
-        startActivity(intent);
+    public void next_screen(View v) {
+        client = new OkHttpClient.Builder()
+                .addInterceptor(new BasicAuthInterceptor("USER_CLIENT_APP", "password"))
+                .build();
+        String username = "training";
+        String password = "password";
+        String grant_type = "password";
+        String requestData = "grant_type=" + grant_type + "&username=" + username + "&password=" + password;
+        try {
+            loginRequest(apURL, requestData);
+            Intent intent = new Intent(this, MainActivity2.class);
+            startActivity(intent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    public void loginRequest(String apUrl, String requestData) throws IOException {
+        RequestBody body = RequestBody.create(CONTENT_TYPE, requestData);
+        Request request = new Request.Builder()
+                .url(apUrl)
+                .method("POST", body)
+                .addHeader("Content-Type", "text/plain")
+                .addHeader("Authorization", "Basic dXNlcmFwcDpwYXNzd29yZA==")
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e("CallBackResponse", "onFailure() Request was: " + call.request());
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                String responseData = response.body().string();
+                JSONObject json = null;
+                try {
+                    json = new JSONObject(responseData);
+                    try {
+                        final String owner = json.getString("access_token");
+
+                        SharedPreferences preferencias = getSharedPreferences("data", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = preferencias.edit();
+                        editor.putString("token", owner);
+                        editor.commit();
+                        finish();
+
+                        System.out.println(owner);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
     }
+
+}
--- app/src/main/java/com/example/technical_test/MainActivity2.java
+++ app/src/main/java/com/example/technical_test/MainActivity2.java
@@ -2,12 +2,17 @@ package com.example.technical_test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.technical_test.Options.BasicAuthInterceptor;
 
@@ -15,6 +20,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.MediaType;
@@ -24,60 +30,81 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class MainActivity2 extends AppCompatActivity {
-
-    private TextView textView2;
-
     public OkHttpClient client;
-    //MediaType mediaType = MediaType.parse("text/plain");
-    //RequestBody body = RequestBody.create(mediaType, "");
-    public static final MediaType CONTENT_TYPE = MediaType.get("text/plain");
-    String apURL= "https://apidev.buroidentidad.com:9425/uaa/oauth/token?grant_type=password&username=training&password=password";
+    private TextView textView2,ETproducto;
+
+    public static final MediaType CONTENT_TYPE = MediaType.get("application/json");
+    String apURL = "https://apidev.buroidentidad.com:9425/bid/rest/v1/operations";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        textView2=(TextView) findViewById(R.id.tv2);
+        textView2 = (TextView) findViewById(R.id.tv2);
+        ETproducto = (TextView) findViewById(R.id.ET_producto);
+        Button btnenviar=(Button) findViewById(R.id.btn_enviar);
 
+        ETproducto.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
 
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
 
+            public void afterTextChanged(Editable s) {
+                btnenviar.setEnabled(s.toString().length() > 0);
+            }
+        });
 
-
-
-        client = new OkHttpClient.Builder()
-                .addInterceptor(new BasicAuthInterceptor("USER_CLIENT_APP", "password"))
-                .build();
-        String username="training";
-        String password="password";
-        String grant_type="password";
-        String requestData= "grant_type="+grant_type+"&username="+username+"&password="+password;
-        try{
-            loginRequest(apURL, requestData);
-        }catch (IOException e){
-            e.printStackTrace();
-        }
 
         acceso();
     }
 
     private void acceso() {
-        SharedPreferences prefe=getSharedPreferences("datos",Context.MODE_PRIVATE);
-        if (prefe.toString().equals("")){
+        SharedPreferences prefe = getSharedPreferences("data", Context.MODE_PRIVATE);
+        String token_store = prefe.getString("token","data");
+        if (prefe.toString().equals("")) {
             textView2.setText("Denegado");
-        }else{
+        } else {
             textView2.setText("Concedido");
+            System.out.println("contenido: "+token_store);
+        }
+    }
+
+
+
+    public void insertarDato(View v) {
+
+        client = new OkHttpClient.Builder()
+                .addInterceptor(new BasicAuthInterceptor("USER_CLIENT_APP", "password"))
+                .build();
+        String producto = ETproducto.getText().toString();
+        if (ETproducto.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(), "Campo Vacio", Toast.LENGTH_SHORT).show();
+        }else {
+            String requestData = "{\n    \"data\": {\n        \"activityStatus\": \"PENDIENTE\",\n        \"activityValue\": \"\",\n        \"code\": \"" + producto + "\",\n        \"data\": \"\",\n        \"productId\": 1,\n        \"secuence\": 1,\n        \"workflowId\": 1\n    },\n    \"metadata\": {\n        \"accuracy\": 16.08,\n        \"deviceInfo\": \"Motorola moto g(6) plus\",\n        \"latitude\": 19.59450382,\n        \"longitude\": -99.02764833,\n        \"timeZoneId\": 1,\n        \"userId\": 1\n    }\n}";
+            try {
+                loginRequest(apURL, requestData);
+                Intent intent = new Intent(this, MainActivity2.class);
+                startActivity(intent);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public void loginRequest(String apUrl, String requestData) throws IOException {
-
+        SharedPreferences prefe = getSharedPreferences("data", Context.MODE_PRIVATE);
+        String token_store = prefe.getString("token","data");
         RequestBody body = RequestBody.create(CONTENT_TYPE, requestData);
         Request request = new Request.Builder()
                 .url(apUrl)
                 .method("POST", body)
-                .addHeader("Content-Type", "text/plain")
-                .addHeader("Authorization", "Basic dXNlcmFwcDpwYXNzd29yZA==")
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Authorization", "Bearer "+token_store)
                 .build();
 
         Call call = client.newCall(request);
@@ -91,23 +118,28 @@ public class MainActivity2 extends AppCompatActivity {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
 
-                    String responseData = response.body().string();
-                JSONObject json = null;
+                String responseData = response.body().string();
+                Log.e("Response","MensajeRes: "+responseData);
+                JSONObject json;
                 try {
                     json = new JSONObject(responseData);
-                    try {
-                        final String owner = json.getString("access_token");
+                    final JSONObject data = json.getJSONObject("data");
+                    Log.e("Mensaje: ","Buscando :"+data);
+                   String operationId=data.getString("operationId");
 
-                        SharedPreferences preferencias=getSharedPreferences("datos",Context.MODE_PRIVATE);
-                        SharedPreferences.Editor editor=preferencias.edit();
-                        editor.putString("token", owner);
-                        editor.commit();
-                        finish();
 
-                        System.out.println(owner);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
+                    Log.e("Response","Mensaje: "+response.toString());
+
+
+                    Log.e("Mensaje: ","Buscando :"+operationId);
+
+
+
+                    SharedPreferences preferencias = getSharedPreferences("data", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferencias.edit();
+                    editor.putString("operationId", operationId);
+                    editor.commit();
+                    finish();
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -115,7 +147,5 @@ public class MainActivity2 extends AppCompatActivity {
 
         });
     }
-
-
 
 }
